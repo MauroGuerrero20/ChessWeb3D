0e6bfa28034e99f5c43c37623fc26705f9fb94ca
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-08-10 00:23:51 +0000

    Remove unnecessary test app component, and test angular features.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,8 +9,16 @@ export class HomeComponent implements OnInit {
 
   clicked = false;
 
+  title:String = 'I have been clicked';
+
+  boat = {
+    name: 'Starfire',
+    year: 1997,
+  };
+
   handleClick = () => {
     this.clicked = true;
+    console.log(this.clicked);
   }
 
   constructor() { }
