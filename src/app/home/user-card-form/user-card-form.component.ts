373e84c0f64a478148cf864138533ebf42b202ec
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-08-12 03:10:15 +0000

    Integrates firestore with user-card component

--- src/app/home/user-card-form/user-card-form.component.ts
+++ src/app/home/user-card-form/user-card-form.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators, } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AngularFirestore } from "@angular/fire/firestore";
+import sha256 from 'crypto-js/sha256';
+
+interface GameRoom {
+  players: string[],
+  roomName: string,
+  roomPassword: string,
+}
 
 @Component({
   selector: 'app-user-card-form',
@@ -20,44 +27,90 @@ export class UserCardFormComponent {
     roomPassword: [null, Validators.required],
   });
 
-  async storeDataFirestore(roomFormData: any) {
+  roomFull = false;
 
-    const gameRoom = {
-      players: [roomFormData.nickname.value],
-      roomName: roomFormData.roomName.value,
-      roomPassword: roomFormData.roomPassword.value,
-    }
-
-    console.log('gameRoom', gameRoom, {gameRoom});
-
-    return await this.firestore.collection('rooms').doc("1234").set(gameRoom)
-      .then(() => {
-        return true; // Room successfully stored
-      })
-      .catch((err) => {
-        console.error(err);
-        return false;
-      });
-
+  getRoomId(gameRoom: GameRoom): string {
+    return sha256(gameRoom.roomName + gameRoom.roomPassword).toString();
   }
 
-  createJoinRoom(): void {
+  async fetchGameRoomFirestore(gameRoom: GameRoom) {
+
+    const docRef = this.firestore.collection('rooms').doc(this.getRoomId(gameRoom))
+
+    const retrivedGameRoom = await docRef.get().toPromise()
+      .then((doc) => {
+        if (doc.exists) {
+          return doc.data();
+        } else {
+          return null;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        return null;
+      });
+
+    return retrivedGameRoom;
+  }
+
+  async storeGameRoomFirestore(gameRoom: GameRoom) {
+
+    const docRef = this.firestore.collection('rooms').doc(this.getRoomId(gameRoom));
+
+    return docRef.set(gameRoom)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  async updateGameRoomFirestore(gameRoom: GameRoom) {
+
+    const docRef = this.firestore.collection('rooms').doc(this.getRoomId(gameRoom));
+
+    return docRef.update({
+      players: gameRoom.players,
+    })
+      .catch(err => console.log(err));
+  }
+
+  async createJoinRoom() {
 
     const nickname = this.roomForm.controls.nickname;
     const roomName = this.roomForm.controls.roomName;
     const roomPassword = this.roomForm.controls.roomPassword;
-
-    console.log('roomForm', this.roomForm, nickname, roomName, roomPassword);
 
     const invalidInput = (nickname.hasError('required') ||
       roomName.hasError('required') ||
       roomPassword.hasError('required'));
 
     if (!invalidInput) {
-      this.storeDataFirestore({ nickname, roomName, roomPassword });
-      this.router.navigate(['/room', 1234]);
+
+      const gameRoom: GameRoom = {
+        players: [nickname.value],
+        roomName: roomName.value,
+        roomPassword: roomPassword.value,
+      }
+
+      const retrivedGameRoom: GameRoom = await this.fetchGameRoomFirestore(gameRoom);
+
+      if (retrivedGameRoom !== null && retrivedGameRoom.players.length < 2) {
+
+        gameRoom.players = [...retrivedGameRoom.players, nickname.value];
+        await this.updateGameRoomFirestore(gameRoom);
+
+        this.router.navigate(['/room', 1234]);
+
+      } else if (retrivedGameRoom !== null && retrivedGameRoom.players.length >= 2) {
+        this.roomFull = true;
+      } else {
+        await this.storeGameRoomFirestore(gameRoom);
+        this.router.navigate(['/room', 1234]);
+      }
+
+      console.log("Hello", await this.fetchGameRoomFirestore(gameRoom))
+
     } else {
-      // Display Error Message
+      // Display Form Error Message
       this.roomForm.markAllAsTouched();
     }
   }
