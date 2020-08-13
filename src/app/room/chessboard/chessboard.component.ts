55a10b26bed23e44cb8a1c47ec477c3f271eedb1
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-08-13 05:09:31 +0000

    Fix Window scroll on Babylon scene. Debug info still present.

--- src/app/room/chessboard/chessboard.component.ts
+++ src/app/room/chessboard/chessboard.component.ts
@@ -2,10 +2,11 @@ import { Component, HostListener, ViewChild, ElementRef, OnInit, Host } from '@a
 import {
   Scene,
   Engine,
-  FreeCamera,
+  ArcRotateCamera,
   Vector3,
   HemisphericLight,
   Mesh,
+  Tools,
 } from "babylonjs";
 
 @Component({
@@ -27,26 +28,24 @@ export class ChessboardComponent implements OnInit {
   createScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
 
     const scene = new Scene(engine);
-    const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene);
 
-    camera.setTarget(Vector3.Zero());
+    const camera = new ArcRotateCamera('camera1', Tools.ToRadians(180), Tools.ToRadians(70), 10, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
     const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene);
+    const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene, false, Mesh.FRONTSIDE);
     sphere.position.y = 1;
 
-    const ground = Mesh.CreateGround('ground1', 6, 6, 2, scene);
+    const ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+
+    // scene.debugLayer.show();
 
     return scene;
-
   }
 
   ngAfterViewInit() {
-
-
     this.canvas = this.canvasRef.nativeElement;
 
     this.engine = new Engine(this.canvas, true, { preserveDrawingBuffer: true, stencil: true });
@@ -56,12 +55,10 @@ export class ChessboardComponent implements OnInit {
     this.engine.runRenderLoop(() => {
       scene.render();
     });
-
   }
 
   @HostListener('window:resize')
   onResize() {
-      console.log("resize called");
     this.engine.resize();
   }
 
