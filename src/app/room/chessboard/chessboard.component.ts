66678d20dbebd84818acf6a2334935de6b02460f
Mauro Guerrero <mauroguerrero20@gmail.com>
2020-08-14 03:57:42 +0000

    Limit Camera Vertical movement

--- src/app/room/chessboard/chessboard.component.ts
+++ src/app/room/chessboard/chessboard.component.ts
@@ -14,6 +14,7 @@ import {
   Tools,
   Vector3,
   Color3,
+  UniversalCamera,
 } from 'babylonjs';
 import { xor } from 'mathjs';
 import { ChessboardServiceService } from './chessboard-service.service';
@@ -73,7 +74,9 @@ export class ChessboardComponent implements OnInit, AfterViewInit {
     let base = 0;
     for (let row = 0; row < boardHeight; row++) {
       for (let col = 0; col < boardWidth; col++) {
-        chessboardGround.subMeshes.push(new SubMesh(Number(xor(row % 2, col % 2 )), 0, verticesCount, base, tileIndicesLength, chessboardGround));
+        chessboardGround.subMeshes.push(
+          new SubMesh(
+            Number(xor(row % 2, col % 2)), 0, verticesCount, base, tileIndicesLength, chessboardGround));
         base += tileIndicesLength;
       }
     }
@@ -85,8 +88,10 @@ export class ChessboardComponent implements OnInit, AfterViewInit {
 
     const scene: Scene = new Scene(engine);
 
-    const camera: ArcRotateCamera = new ArcRotateCamera('camera1', Tools.ToRadians(180), Tools.ToRadians(70), 10, Vector3.Zero(), scene);
-    camera.attachControl(canvas, false);
+    const camera: ArcRotateCamera = new ArcRotateCamera('camera1', Tools.ToRadians(180), Tools.ToRadians(60), 10, Vector3.Zero(), scene);
+    camera.lowerBetaLimit = 0;
+    camera.upperBetaLimit = Tools.ToRadians(70);
+    camera.attachControl(canvas, true);
 
     const light: HemisphericLight = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
